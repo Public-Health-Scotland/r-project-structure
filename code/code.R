207ab9fa07f1814c74b555d9ec9eafa6a8ef5b20
annaprice <anna.price5@nhs.net>
2018-07-10 09:51:12 +0100

    Add basic structure to code.R

--- code/code.R
+++ code/code.R
@@ -1,1 +1,33 @@
+##########################################################
+# Name of file
+# Data release (if applicable)
+# Original author(s)
+# Original date
+# Latest update author (if not using version control)
+# Latest update date (if not using version control)
+# Latest update description (if not using version control)
+# Type of script (e.g. extraction, preparation, modelling)
+# Written/run on (e.g. R Studio SERVER)
+# Version of R that the script was most recently run on
+# Description of content
+# Approximate run time
+##########################################################
 
+
+### 1 - Housekeeping ----
+# This section should be the only section of the script which requires manual changes 
+# for future updates and includes:
+#   loading packages
+#   setting filepaths and extract dates
+#   functions (defined here or sourced from another file)
+#   setting plot parameter
+#   specifying codes (e.g. ICD-10 codes)
+
+
+### 2 Section Heading ----
+
+
+### 3 Section Heading ----
+
+
+### END OF SCRIPT ###
